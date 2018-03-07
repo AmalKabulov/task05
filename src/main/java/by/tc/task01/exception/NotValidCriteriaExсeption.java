57128883976f775550d3ddb4c943e30179390e97
Amal <amal.kabulov@compit.by>
2018-03-07 20:16:53 +0300

    initial commit

--- src/main/java/by/tc/task01/exception/NotValidCriteriaExсeption.java
+++
@@ -1,7 +0,0 @@
-package by.tc.task01.exception;
-
-public class NotValidCriteriaExсeption extends RuntimeException {
-    public NotValidCriteriaExсeption(String message) {
-        super(message);
-    }
-}
